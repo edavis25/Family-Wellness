920d108b5d26aca6319177f4345a8e2ec08d54b8
Eric Davis <davis.25811@gmail.com>
2019-04-18 14:23:34 -0400

    Update 503 view copy

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div style="margin: 100px 0;">
                 <h1 class="title is-1">Our Apologies!</h1>
-                <p class="subtitle is-5">We are currently doing maintenance on our site, please check back later!</p>
+                <p class="subtitle is-5">We are currently doing maintenance on our site, please check back later.</p>
             </div>
         </div>
     </section>
